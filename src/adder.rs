e9a42cfee827401eb685812e9eb3af85bc83a163
roti <rotisant@gmail.com>
2022-03-11 08:22:03 +0530

    asd

--- src/adder.rs
+++ src/adder.rs
@@ -22,22 +22,6 @@ pub trait Adder {
         self.sum().update(|sum| *sum += value);
     }
 
-    // #[view]
-    // fn temp_sum(&self) -> BigUint {
-    //     let biguint_result = self
-    //         .contract_proxy(self.blockchain().get_sc_address())
-    //         .sum()
-    //         .execute_on_dest_context();
-    //     return biguint_result;
-    // }
-
-    // #[endpoint]
-    // fn temp_add(&self, value: BigUint) {
-    //     self.contract_proxy(self.blockchain().get_sc_address())
-    //         .add(value)
-    //         .execute_on_dest_context();
-    // }
-
     // Below Function will transfer all the EGLD balance of contract to the Delegation Contract
     // Make sure your contract has some existing funds
     #[endpoint]
@@ -58,20 +42,6 @@ pub trait Adder {
     #[proxy]
     fn contract_proxy(&self, to: ManagedAddress) -> callee_proxy::Proxy<Self::Api>;
 }
-
-// mod callee_proxy {
-//     elrond_wasm::imports!();
-
-//     #[elrond_wasm::proxy]
-//     pub trait CalleeContract {
-//         #[view(getSum)]
-//         #[storage_mapper("sum")]
-//         fn sum(&self) -> SingleValueMapper<BigUint>;
-
-//         #[endpoint]
-//         fn add(&self, value: BigUint);
-//     }
-// }
 
 mod callee_proxy {
     elrond_wasm::imports!();
